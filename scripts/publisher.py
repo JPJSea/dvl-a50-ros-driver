0714d88c2859520974a696ccc56f1e0c2a162f32
JPJSea <emailJessP@gmail.com>
2021-10-08 14:20:03 +0100

    fix syntax error

--- scripts/publisher.py
+++ scripts/publisher.py
@@ -9,15 +9,15 @@ from waterlinked_a50_ros_driver.msg import DVLBeam
 import select
 
 def connect():
-	global s, TCP_IP, TCP_PORT
-	try:
-		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		s.connect((TCP_IP, TCP_PORT))
-		s.settimeout(1)
-	except socket.error as err:
-		rospy.logerr("No route to host, DVL might be booting? {}".format(err))
-		sleep(1)
-		connect()
+    global s, TCP_IP, TCP_PORT
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((TCP_IP, TCP_PORT))
+        s.settimeout(1)
+    except socket.error as err:
+        rospy.logerr("No route to host, DVL might be booting? {}".format(err))
+        sleep(1)
+        connect()
 
 oldJson = ""
 
@@ -28,42 +28,42 @@ beam2 = DVLBeam()
 beam3 = DVLBeam()
 
 def getData():
-	global oldJson, s
-	raw_data = ""
+    global oldJson, s
+    raw_data = ""
 
-	while not '\n' in raw_data:
-		try:
-			rec = s.recv(1) # Add timeout for that
-			if len(rec) == 0:
-				rospy.logerr("Socket closed by the DVL, reopening")
-				connect()
-				continue
-		except socket.timeout as err:
-			rospy.logerr("Lost connection with the DVL, reinitiating the connection: {}".format(err))
-			connect()
-			continue
-		raw_data = raw_data + rec
-	raw_data = oldJson + raw_data
-	oldJson = ""
-	raw_data = raw_data.split('\n')
-	oldJson = raw_data[1]
-	raw_data = raw_data[0]
-	return raw_data
-	
+    while not '\n' in raw_data:
+        try:
+            rec = s.recv(1) # Add timeout for that
+            if len(rec) == 0:
+                rospy.logerr("Socket closed by the DVL, reopening")
+                connect()
+                continue
+        except socket.timeout as err:
+            rospy.logerr("Lost connection with the DVL, reinitiating the connection: {}".format(err))
+            connect()
+            continue
+        raw_data = raw_data + rec
+    raw_data = oldJson + raw_data
+    oldJson = ""
+    raw_data = raw_data.split('\n')
+    oldJson = raw_data[1]
+    raw_data = raw_data[0]
+    return raw_data
+    
 
 def publisher():
-	pub_raw = rospy.Publisher('dvl/json_data', String, queue_size=10)
-	pub = rospy.Publisher('dvl/data', DVL, queue_size=10)
-	
-	rate = rospy.Rate(10) # 10hz
-	while not rospy.is_shutdown():
-		raw_data = getData()
-		if do_log_raw_data:
-			rospy.loginfo(raw_data)
-		data = json.loads(raw_data)
-		pub_raw.publish(raw_data)
-		
-        if data["type"] == "velocity"
+    pub_raw = rospy.Publisher('dvl/json_data', String, queue_size=10)
+    pub = rospy.Publisher('dvl/data', DVL, queue_size=10)
+    
+    rate = rospy.Rate(10) # 10hz
+    while not rospy.is_shutdown():
+        raw_data = getData()
+        if do_log_raw_data:
+            rospy.loginfo(raw_data)
+        data = json.loads(raw_data)
+        pub_raw.publish(raw_data)
+        
+        if data["type"] == "velocity":
             theDVL.header.stamp = rospy.Time.now()
             theDVL.header.frame_id = "dvl_link"
             theDVL.time = data["time"]
@@ -107,17 +107,17 @@ def publisher():
             theDVL.beams = [beam0, beam1, beam2, beam3]
             
             pub.publish(theDVL)
-		
-		rate.sleep()
+        
+        rate.sleep()
 
 if __name__ == '__main__':
-	global s, TCP_IP, TCP_PORT, do_log_raw_data
-	rospy.init_node('a50_pub', anonymous=False)
-	TCP_IP = rospy.get_param("~ip", "10.42.0.186")
-	TCP_PORT = rospy.get_param("~port", 16171)
-	do_log_raw_data = rospy.get_param("~do_log_raw_data", True)
-	connect()
-	try:
-		publisher()
-	except rospy.ROSInterruptException:
-		s.close()
+    global s, TCP_IP, TCP_PORT, do_log_raw_data
+    rospy.init_node('a50_pub', anonymous=False)
+    TCP_IP = rospy.get_param("~ip", "10.42.0.186")
+    TCP_PORT = rospy.get_param("~port", 16171)
+    do_log_raw_data = rospy.get_param("~do_log_raw_data", True)
+    connect()
+    try:
+        publisher()
+    except rospy.ROSInterruptException:
+        s.close()
